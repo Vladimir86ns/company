e85cd18536db9fa852577c0e96d83263cfcbc301
vladimir86ns <vladimir86ns@gmail.com>
2019-04-30 21:19:44 +0200

    setup

---
+++ src/services/_routerService.js
@@ -0,0 +1,14 @@
+// routes
+import Dashboard from 'Routes/dashboard';
+import Crm from 'Routes/crm';
+
+export default [
+   {
+      path: 'dashboard',
+      component: Dashboard
+   },
+   {
+      path: 'crm',
+      component: Crm
+   }
+]

0693ab5b8b69db5ba0d34effcd9dc67b171df71f
vladimir86ns <vladimir86ns@gmail.com>
2019-07-02 16:05:45 +0200

    add dashboard for task manager as a Trello

--- src/routes/task-manager/task-dashboard/dashboard/index.js
+++ src/routes/task-manager/task-dashboard/dashboard/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import IntlMessages from 'Util/IntlMessages';
-import RctCollapsibleCard from 'Components/RctCollapsibleCard/RctCollapsibleCard';
+import React, { Component } from 'react';
+import {
+    Card,
+    CardText,
+    CardTitle,
+  } from 'reactstrap';
+  import MatButton from '@material-ui/core/Button';
 
-class EmployeeInformationForm extends React.Component {
+class DashBoard extends Component {
     render() {
         return (
-        <div className="textfields-wrapper">
-            <RctCollapsibleCard heading={<IntlMessages id={'form.employee.header'} />}>
-                <h1>Task Manager</h1>
-            </RctCollapsibleCard>
+        <div className="grid-list-wrapper">
+            <div className="row">
+                {['one', 'two', 'three'].map((val, i) => {
+                    return (
+                        <Card 
+                            className="col-sm-3 mr-3" 
+                            style={{backgroundColor: '#F4F7FA'}} 
+                            body 
+                            outline 
+                            color="secondary" 
+                            key={i}>
+                            <CardTitle className="font-weight-bold text-dark text-center">{val}</CardTitle>
+                                {[1,2,3,4,5,6,7].map((val , i) => {
+                                    return (
+                                        <div className="mb-10" ke={i}>
+                                            <Card body outline color="c">
+                                                <CardTitle className="font-weight-bold">Special Title Treatment dsad sadsa sdsa</CardTitle>
+                                            </Card>
+                                        </div>
+                                    );
+                                })}
+                        </Card>
+                    );
+                })}
+                <MatButton variant="fab" color="primary" className="text-white mr-15 mb-10" aria-label="add">
+                    <i className="zmdi zmdi-plus"></i>
+                </MatButton>
+            </div>
         </div>
         );
     }
 }
 
-export default connect(null, null)(EmployeeInformationForm);
+export default DashBoard;
